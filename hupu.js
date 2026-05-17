063bebcc10e968af977b67263b72405ab7d4a32f
evelynnsb <42189455+evelynnsb@users.noreply.github.com>
2026-05-17 19:55:15 +0800

    Update hupu.js

--- hupu.js
+++ hupu.js
@@ -1,18 +1,53 @@
-#!name=HUPU Remove Ads
-#!desc=虎扑去广告合集
-#!author=devin
-#!version=1.0
+/**
+ * @supported Quantumult X
+ * @name HUPU_remove_ads
+ * @description 屏蔽虎扑广告，保留帖子内容
+ * @author ChatGPT
+ * @version 1.0
+ */
 
-[rewrite_local]
+let body = $response.body;
+let url = $request.url;
 
-^https:\/\/fairy\.mobileapi\.hupu\.com\/mang\/preview\/banners url script-response-body https://kelee.one/Resource/JavaScript/HUPU/HUPU_remove_ads.js
+// ---------- 首页广告 ----------
+if (/cnode\.hupu\.com.*ad/.test(url)) {
+    // 直接返回空 JSON
+    body = JSON.stringify({});
+}
 
-^https:\/\/bbs\.mobileapi\.hupu\.com\/\d\/\d\.\d\.\d+\/bbsallapi\/lego\/data url script-response-body https://kelee.one/Resource/JavaScript/HUPU/HUPU_remove_ads.js
+// ---------- 资讯/帖子列表广告 ----------
+if (/bbs\.hupu\.com.*ad/.test(url)) {
+    let obj = JSON.parse(body);
+    // 删除帖子列表中的广告项
+    if (obj && obj.list) {
+        obj.list = obj.list.filter(item => !item.ad);
+    }
+    body = JSON.stringify(obj);
+}
 
-^https:\/\/games\.mobileapi\.hupu\.com\/\d\/\d\.\d\.\d+\/buffer\/hotList url script-response-body https://kelee.one/Resource/JavaScript/HUPU/HUPU_remove_ads.js
+// ---------- 视频广告 ----------
+if (/video\.hupu\.com.*ad/.test(url)) {
+    body = JSON.stringify({});
+}
 
-^https:\/\/games\.mobileapi\.hupu\.com\/\d\/\d\.\d\.\d+\/bplapi\/user\/v1\/more url script-response-body https://kelee.one/Resource/JavaScript/HUPU/HUPU_remove_ads.js
+// ---------- Banner 广告 ----------
+if (/img\d*\.hupu\.com.*banner/.test(url)) {
+    body = '';
+}
 
-[mitm]
+// ---------- 插屏/弹窗广告 ----------
+if (/ad\.hupu\.com/.test(url)) {
+    body = '';
+}
 
-hostname = fairy.mobileapi.hupu.com, bbs.mobileapi.hupu.com, games.mobileapi.hupu.com
+// ---------- 推荐/猜你喜欢广告接口 ----------
+if (/api\.hupu\.com.*recommend.*ad/.test(url)) {
+    let obj = JSON.parse(body);
+    if (obj && obj.items) {
+        obj.items = obj.items.filter(item => !item.ad);
+    }
+    body = JSON.stringify(obj);
+}
+
+// 返回修改后的响应体
+$done({ body });
